d388eb4b1971f731febcf155a0dce7971ecf1358
Zeynep <60406089+Elkaisan@users.noreply.github.com>
2025-01-15 11:51:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("scrollButton");
+button.addEventListener("click", () => {
+  document.getElementById("projects").scrollIntoView();
+});
+
+// function openInNewTab(url) {
+//   window.open(url, "_blank");
+// }
